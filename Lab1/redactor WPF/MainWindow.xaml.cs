203ae4fa2a1f2bf4a63c7ba49063a02be62fe573
Dmytrii-Pylypchuk <dimapilipchuk@knu.ua>
2026-02-23 08:29:54 +0200

    Adding files 4 lab1
    here r the files, that needed to start and check the program 4 lab1

---
+++ Lab1/redactor WPF/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+
+namespace redactor_WPF
+{
+    public partial class MainWindow : Window
+    {
+        private string currentFilePath = "";
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void NewFile_Click(object sender, RoutedEventArgs e)
+        {
+            Editor.Clear();
+            currentFilePath = "";
+        }
+
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text files (*.txt)|*.txt";
+
+            if (ofd.ShowDialog() == true)
+            {
+                currentFilePath = ofd.FileName;
+                Editor.Text = File.ReadAllText(currentFilePath);
+            }
+        }
+
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Text files (*.txt)|*.txt";
+
+                if (sfd.ShowDialog() == true)
+                    currentFilePath = sfd.FileName;
+            }
+
+            if (!string.IsNullOrEmpty(currentFilePath))
+                File.WriteAllText(currentFilePath, Editor.Text);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
---
+++ Lab1/redactor/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace redactor
+{
+    public partial class Form1 : Form
+    {
+        private string currentFilePath = "";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // можна залишити порожнім
+        }
+        private void NewFile_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            currentFilePath = "";
+        }
+
+        private void OpenFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                currentFilePath = ofd.FileName;
+                textBox1.Text = File.ReadAllText(currentFilePath);
+            }
+        }
+
+        private void SaveFile_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(currentFilePath))
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Text files (*.txt)|*.txt";
+
+                if (sfd.ShowDialog() == DialogResult.OK)
+                    currentFilePath = sfd.FileName;
+            }
+
+            if (!string.IsNullOrEmpty(currentFilePath))
+                File.WriteAllText(currentFilePath, textBox1.Text);
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
