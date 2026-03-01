82caae57bb9b8094ff6f2c1e23389715b69216eb
Dmytrii-Pylypchuk <dimapilipchuk@knu.ua>
2026-03-01 21:08:43 +0200

    Delete preload.js

--- preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-    loadFile: (callback) => ipcRenderer.on('load-file', (e, data) => callback(data)),
-    clearEditor: (callback) => ipcRenderer.on('clear-editor', callback),
-    notifyChange: () => ipcRenderer.send('content-changed'),
-    saveToFile: (content) => ipcRenderer.invoke('save-to-file', content)
-});
